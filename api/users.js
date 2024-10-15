b48d2fa5e26658c7604c6d4379f035babc47a87e
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-15 19:11:55 -0400

    Initialized prisma, created and connected to a db, defined model, seeded the db, added GET /users

--- api/users.js
+++ api/users.js
@@ -2,14 +2,25 @@ const express = require("express");
 const router = express.Router();
 const prisma = require("../prisma/client");
 
-router.get("/users", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   console.log("reached /users");
   try {
     const users = await prisma.user.findMany();
-    res.send("users");
+    res.json(users);
   } catch (e) {
     console.log(e);
   }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { id: +id },
+      include: { playlists: true },
+    });
+    res.json(user);
+  } catch (e) {}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 app.use(express.json());
 //app.use(require("morgan")("dev"));
+
+app.use("", require("./api/users.js"));
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}`);
   next();
-});
-
-app.use((req, res, next) => {
-  next({ status: 404, message: "Endpoint not found." });
 });
 
 app.use((err, req, res, next) => {
@@ -19,7 +17,9 @@ app.use((err, req, res, next) => {
   res.json(err.message ?? "Sorry, something went wrong :(");
 });
 
-app.use("/users", require("./api/users.js"));
+/* app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint not found." });
+}); */
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
